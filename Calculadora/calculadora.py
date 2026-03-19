208d6ffdf4b937ba06bd21a87fdfa5363d9a54cc
Yuri de Sousa Dias <diasyuri752@gmail.com>
2026-03-19 13:03:15 +0000

    .

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,2 +1,22 @@
+import soma
+import subtracao
 
-print("Informe a operação desejada:\n1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao")
+aux=int(input("Informe a operação desejada:\n1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao\n"))
+if(aux==1):
+    resultado=int(soma.soma())
+    print(f"Resultado: {resultado}")
+elif(aux==2):
+    resultado=int(subtracao.subtracao())
+    print(f"Resultado: {resultado}")
+elif(aux==3):
+    resultado=int(multiplicacao.multiplicacao())
+    print(f"Resultado: {resultado}")
+elif(aux==4):
+    resultado=int(divisao.divisao())
+    print(f"Resultado: {resultado}")
+
+
+
+
+
+
--- Calculadora/subtracao.py
+++ Calculadora/subtracao.py
@@ -1,7 +1,7 @@
-def subtracao (a,b):
-  a= float(input('numero 1: '))
-  b= float(input('numero 2: '))
+def subtracao ():
+  a= float(input('Numero 1: '))
+  b= float(input('Numero 2: '))
   subtracao= a - b
-  return(subtracao)
+  return subtracao
 
-print(subtracao(1,2))
+
